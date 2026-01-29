9cd80e7a8c7aabbb0194d9dba8c591fdcaa3c59c
Karishma Nair <karishmanair@Karishmas-MacBook-Air.local>
2026-01-30 04:12:13 +0530

    Initial commit: Full stack ExpenseFlow with Analytics and Currency API 2

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ks+#fqgwwk9&@%x9c8w=@#7^1e7ow65dqdz6nbvg0d7a9mnk_l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
